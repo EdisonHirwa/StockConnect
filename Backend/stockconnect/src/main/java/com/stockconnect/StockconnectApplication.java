e4905c3864cd100192ed76ed054dd865f1d2a0bb
Edison <hirwae15@gmail.com>
2026-02-26 22:25:45 +0200

    Feat:project structure

---
+++ Backend/stockconnect/src/main/java/com/stockconnect/StockconnectApplication.java
@@ -0,0 +1,13 @@
+package com.stockconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockconnectApplication.class, args);
+	}
+
+}
---
+++ Backend/stockconnect/src/test/java/com/stockconnect/StockconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
